3462fb8738231bb1a7786793a72fa6439f4647d5
Christopher Lum <chrlum03@gmail.com>
2024-12-04 21:57:24 -0800

    add parameters

--- run.py
+++ run.py
@@ -11,17 +11,32 @@ from ObjectivePerturbation import ObjPert
 
 def main(targets):
     with open("params.json") as fh:
-        data_params = json.load(fh)
+        params = json.load(fh)
 
     all_methods = ["gd", "sgd", "ftrlm", "objpert", "outpert"]
+    all_objects = [GD, SGD, FTRLM, ObjPert]
 
     # if no target is specified, run all methods
     target_methods = set(all_methods).intersection(set(targets))
     if len(set(all_methods).intersection(set(targets))) == 0:
         target_methods = all_methods
 
-    if "gd" in target_methods:
-        gd = GD(fp=data_params["fp"])
+    combined_df = []
+    for method, obj in zip(all_methods, all_objects):
+        if method not in target_methods:
+            continue
+        specific_params = {
+            "fp": params["fp"],
+            "epsilons": params["epsilons"],
+            "delta": params["delta"],
+        }
+        if method == "objpert":
+            specific_params["trials"] = params["ObjPert_trials"]
+            specific_params["repeats"] = params["ObjPert_repeats"]
+
+        run = obj(**specific_params)
+        combine_df = run.run_all_plots()
+        combined_df.append(combine_df)
 
 
 if __name__ == "__main__":
