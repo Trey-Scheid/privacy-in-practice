52813418310d23dfedcf8d3b0e9300126a95dff3
Christopher Lum <chrlum03@gmail.com>
2025-03-09 16:28:12 -0700

    fix netflix path again

--- src/components/what-is-dp/centered/CenteredView.tsx
+++ src/components/what-is-dp/centered/CenteredView.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { HistogramViz } from "./histogram/HistogramViz";
 import { ConfidenceViz } from "./confidence/ConfidenceViz";
 import { useState } from "react";
-import { getPublicPath } from "@/lib/utils";
 
 export function CenteredView() {
   const [showRevealDetails, setShowRevealDetails] = useState(false);
