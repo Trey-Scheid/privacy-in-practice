fd161b102887a898766e1287aec64c574963962a
Christopher Lum <chrlum03@gmail.com>
2025-03-09 03:26:44 -0700

    fix favicon pathing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export const metadata: Metadata = {
   description: "The Feasibility of Differential Privacy for Telemetry Analysis",
   icons: {
     icon: [
-      { url: "/favicons/favicon.ico" },
-      { url: "/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicons/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
   },
 };
